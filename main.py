36affc04fb1979afe1ffd92b312978b3c8b18e09
JeanCharlesBertrand <jeancharlesbertrand@outlook.com>
2020-11-26 17:03:02 -0500

    database not working

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import time
 import datetime
 import tkinter as tk
 from tkinter import messagebox as msg
+from tkinter import ttk
+import sqlite3
+import os
+import functools
 
 
 class CountingThread(threading.Thread):
@@ -41,20 +45,81 @@ class CountingThread(threading.Thread):
             mins, secs = divmod(time_difference.seconds, 60)
             tenth = str(time_difference.microseconds / 1000000.0)[2:3]
             time_string = "{:02d}:{:02d}".format(mins, secs)
-            time_string = f'{time_string}:{tenth}'
+            # time_string = f'{time_string}:{tenth}'
             if not self.force_quit:
                 self.master.update_time_remaining(time_string)
+
+
+class LogWindow(tk.Toplevel):
+    def __init__(self, master):
+        super().__init__()
+
+        self.title("Log")
+        self.geometry("600x300")
+
+        self.notebook = ttk.Notebook(self)
+
+        dates_sql = "SELECT DISTINCT date FROM pymodoros ORDER BY date DESC"
+        dates = self.master.runQuery(dates_sql, None, True)
+
+        for index, date in enumerate(dates):
+            dates[index] = date[0].split()[0]
+
+        dates = sorted(set(dates), reverse=True)
+
+        for date in dates:
+            tab = tk.Frame(self.notebook)
+
+            columns = ("name", "finished", "time")
+
+            tree = ttk.Treeview(tab, columns=columns, show="headings")
+
+            tree.heading("name", text="Name")
+            tree.heading("finished", text="Full 25 Minutes")
+            tree.heading("time", text="Time")
+
+            tree.column("name", anchor="center")
+            tree.column("finished", anchor="center")
+            tree.column("time", anchor="center")
+
+            tasks_sql = "SELECT * FROM pymodoros WHERE date LIKE ?"
+            date_like = date + '%'
+            data = (date_like,)
+
+            tasks = self.master.runQuery(tasks_sql, data, True)
+
+            for task_name, task_finished, task_date in tasks:
+                task_finished_text = "Yes" if task_finished else "No"
+                task_time = task_date.split()[1]
+                task_time_pieces = task_time.split(":")
+                task_time_pretty = "{}:{}".format(task_time_pieces[0], task_time_pieces[1])
+                tree.insert("", tk.END, values=(task_name, task_finished_text, task_time_pretty))
+
+            tree.pack(fill=tk.BOTH, expand=1)
+
+            self.notebook.add(tab, text=date)
+
+        self.notebook.pack(fill=tk.BOTH, expand=1)
 
 
 class Timer(tk.Tk):
     def __init__(self):
         super().__init__()
+        self.task_started_time = None
 
         self.title("Pomodoro Timer")
         self.geometry("500x300")
         self.resizable(False, False)
 
         self.standard_font = (None, 16)
+
+        self.menubar = tk.Menu(self, bg="lightgray", fg="black")
+
+        self.log_menu = tk.Menu(self.menubar, tearoff=0, bg="lightgray", fg="black")
+        self.log_menu.add_command(label="View Log", command=self.show_log_window, accelerator="Ctrl+L")
+
+        self.menubar.add_cascade(label="Log", menu=self.log_menu)
+        self.configure(menu=self.menubar)
 
         self.main_frame = tk.Frame(self, width=500, height=300, bg="lightgray")
         self.task_name_label = tk.Label(self.main_frame, text="Task Name:", bg="lightgray", fg="black", font=self.standard_font)
@@ -74,17 +139,24 @@ class Timer(tk.Tk):
         self.pause_button.pack(fill=tk.X, padx=50)
 
         self.protocol("WM_DELETE_WINDOW", self.safe_destroy)
+        self.bind("<Control-l>", self.show_log_window)
 
     def setup_worker(self):
         now = datetime.datetime.now()
-        in_25_mins = now + datetime.timedelta(minutes=25)
-        # in_25_mins = now + datetime.timedelta(minutes=1)
+        # in_25_mins = now + datetime.timedelta(minutes=25)
+        in_25_mins = now + datetime.timedelta(minutes=1)
         worker = CountingThread(self, now, in_25_mins)
         self.worker = worker
 
     def start(self):
+        if not self.task_name_entry.get():
+            msg.showerror("No Task", "Please enter a task name")
+            return
+
         if not hasattr(self, "worker"):
             self.setup_worker()
+
+        self.task_finished_early = False
 
         self.task_name_entry.configure(state="disabled")
         self.start_button.configure(text="Finish", command=self.finish_early)
@@ -104,6 +176,7 @@ class Timer(tk.Tk):
 
     def finish_early(self):
         self.start_button.configure(text="Start", command=self.start)
+        self.task_finished_early = True
         self.worker.end_now = True
 
     def finish(self):
@@ -111,12 +184,28 @@ class Timer(tk.Tk):
         self.time_remaining_var.set("25:00")
         self.pause_button.configure(text="Pause", state="disabled")
         self.start_button.configure(text="Start", command=self.start)
+        if not self.task_finished_early:
+            self.mark_finished_task()
         del self.worker
         msg.showinfo("Pomodoro Finished!", "Task completed, take a break!")
 
     def update_time_remaining(self, time_string):
         self.time_remaining_var.set(time_string)
         self.update_idletasks()
+
+    def add_new_task(self):
+        task_name = self.task_name_entry.get()
+        self.task_started_time = datetime.datetime.now()
+        add_task_sql = "INSERT INTO pymodoros VALUES (?, 0, ?)"
+        self.runQuery(add_task_sql, (task_name, self.task_started_time))
+
+    def mark_finished_task(self):
+        task_name = self.task_name_entry.get()
+        add_task_sql = "UPDATE pymodoros SET finished = ? WHERE task = ? and date = ?"
+        self.runQuery(add_task_sql, ("1", task_name, self.task_started_time))
+
+    def show_log_window(self, event=None):
+        LogWindow(self)
 
     def safe_destroy(self):
         if hasattr(self, "worker"):
@@ -125,7 +214,32 @@ class Timer(tk.Tk):
         else:
             self.destroy()
 
+    @staticmethod
+    def runQuery(sql, data=None, receive=False):
+        conn = sqlite3.connect("pymodoro.db")
+        cursor = conn.cursor()
+        if data:
+            cursor.execute(sql, data)
+        else:
+            cursor.execute(sql)
+
+        if receive:
+            return cursor.fetchall()
+        else:
+            conn.commit()
+
+        conn.close()
+
+    @staticmethod
+    def firstTimeDB():
+        create_tables = "CREATE TABLE pymodoros (task text, finished integer, date text)"
+        Timer.runQuery(create_tables)
+
 
 if __name__ == "__main__":
     timer = Timer()
+
+    if not os.path.isfile("pymodoro.db"):
+        timer.firstTimeDB()
+
     timer.mainloop()
